6c7867ef4bd527a7effee5a8a0d5d315e5cd91ee
agune001 <ca_adi@yahoo.com>
2015-04-28 21:47:01 -0400

    Data validation for min and max vars
    Added data validation to ensure min multiplier/multiplicand are not
    larger than max.

--- src/multtable.php
+++ src/multtable.php
@@ -6,5 +6,33 @@
 </head>
 
 <body>
+<?php
+$min_multiplicand = 1;
+$max_multiplicand = 1;
+$min_multiplier = 1;
+$max_multiplier = 1;
+$errorflag = false;
+
+foreach($_GET as $min_multiplicand => $max_multiplicand){
+		if($min_multiplicand > $max_multiplicand){
+		echo "Error: Minimum multiplicand larger than maximum";
+		$errorflag = true;
+		}
+}
+
+foreach($_GET as $min_multiplier => $max_multiplier){
+	if($min_multiplier > $max_multiplier){
+		echo "Error: Minimum multiplier larger than maximum";
+		$errorflag = true;
+		}
+	
+}
+
+
+
+
+?>
+
+
 </body>
 </html>
